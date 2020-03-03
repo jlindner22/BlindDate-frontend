c1a26dd6dfb09d61f49378f96a7bcb45c600b10d
jlindner22 <jenlindner22@gmail.com>
2020-03-03 11:00:44 -0500

    name sort working

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -7,47 +7,56 @@ class UserContainer extends React.Component {
 
   state = {
     nameSorted: false,
-    ageSorted: false
+    ageSorted: false,
+    nameButtonClicked: false,
+    ageButtonClicked: false,
+    // namesAZ: this.props.profiles,
+    // ageAscending: this.props.profiles
   }
 
   componentDidMount() {
     window.scrollTo(0, 0)
     this.props.getPreferences()
-    console.log(this.state.nameSorted)
   }
 
   filterLink = () => {
     this.props.history.push('/filters')
   }
 
-//   nameSort = () => {
-//   let sorted = this.props.profiles.map(user => user.name).sort() 
-//   console.log(sorted)
-//     return sorted
-// }
-
-
-// if (this.state.nameSorted === true) {
-//   let sorted = this.props.profiles.map(user => user.name).sort() 
-//   console.log(sorted)
-//     return sorted;
-//   } else {
-//       let notSorted = this.props.profiles.map(user => user.name)
-//       console.log(notSorted)
-//         return notSorted;
-//       }
-
   handleNameSort = () => {
-    this.setState({nameSorted: !this.state.nameSorted})
+    this.setState({
+      // namesAZ: this.props.profiles.sort((a, b) => {
+      //   if (a.name.toLowerCase() < b.name.toLowerCase()) {
+      //     return 1;
+      //   } if (a.name.toLowerCase() > b.name.toLowerCase()) {
+      //     return -1;
+      //   }
+      //   return 0;
+      // }),
+      nameSorted: !this.state.nameSorted,
+      nameButtonClicked: true
+    })
+    if (this.state.nameSorted === false) {
+      this.renderNameSortedList()
+    } else {
+      this.renderReverseNameSortedList()
+    }
   }
 
-
-  handleAgeSort = () => {
-    this.setState({ageSorted: !this.state.ageSorted}, () => console.log(this.state.ageSorted))
-  }
+  // handleAgeSort = () => {
+  //   this.setState({
+  //     ageAscending: this.props.profiles.sort(),
+  //     ageSorted: !this.state.ageSorted,
+  //     ageButtonClicked: true
+  //   })
+  //   if (this.state.ageSorted === false) {
+  //     this.renderYoungToOld()
+  //   } else {
+  //     this.renderReverseSortedAge()
+  //   }
+  // }
 
   renderNameSortedList = () => {
-    console.log("user container props", this.props)
     let nameOrder = this.props.profiles.sort((a, b) => {
       if (a.name.toLowerCase() < b.name.toLowerCase()) {
         return 1;
@@ -127,87 +136,78 @@ renderReverseNameSortedList = () => {
   }))
 }
 
-renderYoungToOld() {
-  let sortedAge = this.props.profiles.sort()
-  //   (a, b) => {
-  //   if (a.age > b.age) {
-  //     return 1;
-  //   } if (a.age < b.age) {
-  //     return -1;
-  //   }
-  //   return 0;
-  // })
-   console.log("sorted age", sortedAge)
-  return (this.props.profiles && sortedAge.map(profile => {
-    return (
-      <div className="card">
-    <div className="image">
-      <img className="ui image" src={profile.avatar} alt="Try again later!" />
-    </div>
-    <div className="content">
-      <a className="header">{profile.name}</a>
-      <div className="meta">
-        <span className="date">Age {profile.age} 
-      </span>
-      </div>
-      <div className="description">
-        {profile.name} lives in {profile.city}, {profile.state}.
-      </div>
-    </div>
-    <div className="extra content"><Link to={`/users/${profile.id}`}> 
-      <button onClick={() => this.props.viewProfile(profile)}
-              className="ui pink basic button">
-                View Profile!
-      </button></Link>
-      <div className="ui right floated">
-      {profile.gender !== "Female" ? <i className="mars icon" ></i>  :  <i className="venus icon"></i> }
-      </div>
-    </div>
-  </div>
-    );
-  }))
-}
+// renderYoungToOld() {
+//   let sortedAge = this.props.profiles.sort()
+//    console.log("sorted age", sortedAge)
+//   return (this.props.profiles && sortedAge.map(profile => {
+//     return (
+//       <div className="card">
+//     <div className="image">
+//       <img className="ui image" src={profile.avatar} alt="Try again later!" />
+//     </div>
+//     <div className="content">
+//       <a className="header">{profile.name}</a>
+//       <div className="meta">
+//         <span className="date">Age {profile.age} 
+//       </span>
+//       </div>
+//       <div className="description">
+//         {profile.name} lives in {profile.city}, {profile.state}.
+//       </div>
+//     </div>
+//     <div className="extra content"><Link to={`/users/${profile.id}`}> 
+//       <button onClick={() => this.props.viewProfile(profile)}
+//               className="ui pink basic button">
+//                 View Profile!
+//       </button></Link>
+//       <div className="ui right floated">
+//       {profile.gender !== "Female" ? <i className="mars icon" ></i>  :  <i className="venus icon"></i> }
+//       </div>
+//     </div>
+//   </div>
+//     );
+//   }))
+// }
 
-
-renderReverseSortedAge = () => {
-  let reverseSortedAge = this.props.profiles.sort((a, b) => {
-    if (a.age < b.age) {
-      return 1;
-    } if (a.age > b.age) {
-      return -1;
-    }
-    return 0;
-  })
-   console.log("reverse sorted age", reverseSortedAge)
-  return (this.props.profiles && reverseSortedAge.map(profile => {
-    return (
-      <div className="card">
-    <div className="image">
-      <img className="ui image" src={profile.avatar} alt="Try again later!" />
-    </div>
-    <div className="content">
-      <a className="header">{profile.name}</a>
-      <div className="meta">
-        <span className="date">Age {profile.age} 
-      </span>
-      </div>
-      <div className="description">
-        {profile.name} lives in {profile.city}, {profile.state}.
-      </div>
-    </div>
-    <div className="extra content"><Link to={`/users/${profile.id}`}> 
-      <button onClick={() => this.props.viewProfile(profile)}
-              className="ui pink basic button">
-                View Profile!
-      </button></Link>
-      <div className="ui right floated">
-      {profile.gender !== "Female" ? <i className="mars icon" ></i>  :  <i className="venus icon"></i> }
-      </div>
-    </div>
-  </div>
-    );
-  }))
-}
+// renderReverseSortedAge = () => {
+//   let reverseSortedAge = this.props.profiles.sort((a, b) => {
+//     if (a.age < b.age) {
+//       return 1;
+//     } if (a.age > b.age) {
+//       return -1;
+//     }
+//     return 0;
+//   })
+//    console.log("reverse sorted age", reverseSortedAge)
+//   return (this.props.profiles && reverseSortedAge.map(profile => {
+//     return (
+//       <div className="card">
+//     <div className="image">
+//       <img className="ui image" src={profile.avatar} alt="Try again later!" />
+//     </div>
+//     <div className="content">
+//       <a className="header">{profile.name}</a>
+//       <div className="meta">
+//         <span className="date">Age {profile.age} 
+//       </span>
+//       </div>
+//       <div className="description">
+//         {profile.name} lives in {profile.city}, {profile.state}.
+//       </div>
+//     </div>
+//     <div className="extra content"><Link to={`/users/${profile.id}`}> 
+//       <button onClick={() => this.props.viewProfile(profile)}
+//               className="ui pink basic button">
+//                 View Profile!
+//       </button></Link>
+//       <div className="ui right floated">
+//       {profile.gender !== "Female" ? <i className="mars icon" ></i>  :  <i className="venus icon"></i> }
+//       </div>
+//     </div>
+//   </div>
+//       );
+//     }))
+//   }
 
   render() {
     return (
@@ -228,16 +228,17 @@ renderReverseSortedAge = () => {
       <button className="ui pink button" onClick={this.handleNameSort} >
         {this.state.nameSorted === false ? "Sort Z -> A" : "Sort A -> Z"}
       </button>
-      <button className="ui pink button" onClick={this.handleAgeSort}>
-      {this.state.ageSorted === false ? "Sort oldest to youngest" : "Sort yougest to oldest"}
-      </button>
+      {/* <button className="ui pink button" onClick={this.handleAgeSort}>
+        {this.state.ageSorted === false ? "Sort age descending" : "Sort age ascending"}
+      </button> */}
       <br></br>
       <br></br>
       <br></br>
       <div className="ui centered row">
         <div className="ui link cards">
-        {this.state.nameSorted === true ? this.renderNameSortedList() : this.renderReverseNameSortedList()}
-        {this.state.ageSorted === false ? this.renderYoungToOld() : this.renderReverseSortedAge()}
+        {this.state.ageButtonClicked === false && this.state.nameSorted === true ? this.renderNameSortedList() : this.renderReverseNameSortedList()}
+        {/* {this.state.nameButtonClicked === false && this.state.ageButtonClicked === true && this.state.ageSorted === true? this.renderYoungToOld() : this.renderReverseSortedAge()}
+        {this.state.ageButtonClicked === true && this.state.nameSorted === false ? this.renderYoungToOld() : this.renderReverseSortedAge()} */}
         </div>
       </div>
       </div>
