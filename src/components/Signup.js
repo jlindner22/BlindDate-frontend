c334f193a048a34d4d01a2686e032d2e3672f9bc
jlindner22 <jenlindner22@gmail.com>
2020-07-27 16:27:13 -0400

    fixed security vulnerabilities

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,8 +7,7 @@ class Signup extends React.Component {
 
     componentDidMount() {
         window.scrollTo(0, 0);
-        // this.props.changeBackgroundToTeal();
-      }
+    }
 
     state = {
         username: '',
